1a3da1253123dc99368402c4620a60a4f8fad1af
YeonBot <tjdds1109@naver.com>
2019-07-16 22:06:22 +0900

    #7 [antd 라이브러리] '.less' 파일 import
    - `import '/antd/dist/antd.less'` 경로 따라서 가 보면 '.less' 파일을 임포트 하는 것을 알 수 있다.
    - 해당 파일 내부에 '@import' 를 컴파일 하기 위해 npm 'style-loader', 'css-loader', 'less-loader' 을 추가.
    - npm 추가 후에 webpack 에서 스타일 파일들 관리 하기 위한 설정.

--- client/categories/sample.jsx
+++ client/categories/sample.jsx
@@ -1,1 +1,16 @@
-asd
+import React , { Component } from 'react';
+import {Button} from "antd";
+
+class Categorie extends Component {
+
+  render() {
+    return (
+        <>
+          <h1>Categories</h1>
+          <Button type="primary">Example button</Button>
+        </>
+    );
+  }
+}
+
+export default Categorie;
--- client/index.jsx
+++ client/index.jsx
@@ -1,12 +1,19 @@
-import React ,{ Component } from 'react';
+import React, {Component} from 'react';
+import Categorie from './categories/sample';
+import 'antd/dist/antd.less';
 
 class Index extends Component {
   state = {
-    text: "hello, webpack!",
+    text: "hello,!",
   };
 
   render() {
-    return <h1>{this.state.text}</h1>;
+    return (
+        <>
+          <h1>{this.state.text}</h1>
+          <Categorie />
+        </>
+    );
   }
 }
 
