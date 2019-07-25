f88c82270692946db077e8daeb48bd1ba1c1b9f7
YeonBot <tjdds1109@naver.com>
2019-07-25 10:42:00 +0900

    #19 footer Component 로 만들어서 빼기
    - Footer.css , Foot.jsx 추가 ( Footer 파일명 생성 불가 )
    - DashBoard Header,Footer 빼고 Component 로 추가
    - root 에 Head , Foot Component 이용 코드 작성

--- client/src/client/root.jsx
+++ client/src/client/root.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { Row, Col } from 'antd';
+import {Row, Col, Layout, Button, Icon} from 'antd';
+const { Footer } = Layout;
 
 import App from '../shared/app';
+import Head from '../component/all/Head';
+import Foot from '../component/all/Foot';
 
 import 'antd/dist/antd.less';
 import '../static/style/total.css';
+import '../static/style/footer.css';
 
 const Root = () => (
     <BrowserRouter>
@@ -13,9 +17,17 @@ const Root = () => (
             <Col span={6}></Col>
 
             <Col span={12}>
+              <Layout className="layout">
+
+                <Head/>
+
                 <App />
+
+                <Foot/>
+
+              </Layout>
             </Col>
-            
+
             <Col span={6}></Col>
         </Row>
     </BrowserRouter>
---
+++ client/src/component/all/Foot.jsx
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import {Button, Icon, Layout} from "antd";
+
+const {Footer} = Layout;
+
+class Foot extends Component {
+
+  render() {
+    return (
+        <>
+          <Footer id="footer" style={{textAlign: 'center'}}>
+            <Button type="link" size="large" ghost>
+              <Icon type="home" theme="twoTone" />
+            </Button>
+            <Button type="link" size="large" ghost>
+              <Icon type="bars" twoToneColor="#eb2f96"/>
+            </Button>
+            <Button type="link" size="large" ghost>
+              <Icon type="smile" theme="twoTone" />
+            </Button>
+            <Button type="link" size="large" ghost>
+              <Icon type="heart" theme="twoTone" twoToneColor="#eb2f96" />
+            </Button>
+            <Button type="link" size="large" ghost>
+              <Icon type="bar-chart"  twoToneColor="#eb2f96"/>
+            </Button>
+          </Footer>
+        </>
+    );
+  }
+}
+
+export default Foot;
