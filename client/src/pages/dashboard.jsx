e08d1c69090c0c3ff12f5b1c953026f7a647e1e9
YeonBot <tjdds1109@naver.com>
2019-07-29 14:26:52 +0900

    #39 코드 리뷰 받은 부분 변경
    - dashboard css 코드 제거
    - index.html viewpoart meta Tag 주석 추가

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -1,9 +1,5 @@
 import React, {Component} from 'react';
-<<<<<<< HEAD
 import { Layout, Breadcrumb } from 'antd';
-=======
-import {Layout, Breadcrumb} from 'antd';
->>>>>>> #39 코드 리팩토링
 
 import Calendar from '../component/dashboard/Calendar';
 import Categories from '../component/dashboard/Categories';
@@ -15,19 +11,10 @@ class Dashboard extends Component {
   render() {
     return (
         <>
-
-            <Content style={{padding: '0 50px'}}>
-
-              <Breadcrumb style={{margin: '16px 0'}}>
-                <Breadcrumb.Item>MoneyDog</Breadcrumb.Item>
-                <Breadcrumb.Item> App List</Breadcrumb.Item>
-              </Breadcrumb>
-
+            <Content>
               <Calendar/>
               <Categories/>
-
             </Content>
-
         </>
     );
   }
