636ebf8fb824e0ab4c45167e660bfee6ef401aae
parkyounghwan <dudrnxps1@gmail.com>
2019-07-31 16:31:28 +0900

    #52 App(Content) 부분 height 100% 적용

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,4 +3,3 @@ export {default as Report} from './report';
 export {default as Dashboard} from './dashboard';
 export {default as Info} from './info';
 export {default as SignUp} from './signup';
-
---
+++ client/src/pages/user/subscriptions.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Subscriptions extends Component {
+  render() {
+    return (
+      <>
+        <h1>Subscriptions</h1>
+      </>
+    );
+  }
+}
+
+export default Subscriptions;
