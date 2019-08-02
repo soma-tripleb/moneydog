82a26feff4c47a7407f12906d8e8736460fa43d0
YeonBot <tjdds1109@naver.com>
2019-08-02 15:28:14 +0900

    #50 node서버 초기 설정
    - /db/mongoDB  mongoDB 연결.
    - /routes/index.js 페이지랜더링 대신, rest 하게 코드 변경
    - .pug 파일들 위에 extend layout 제거
    - .babelrc 파일 생성
    - app.js db연결 부분 import
    - app.js apiRouter 필요 없는 부분 제거
    - packages nodemon , babel 시작 script 작성
    - packages dotenv, mongoose, babel 설정 install
    
    .env 파일 생성후 db 정보 입력 필요

---
+++ server/db/mongoDB.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+const MONGO_URI = `${process.env.DB_SCHEMA}${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}`;
+
+const conn = mongoose.createConnection(MONGO_URI);
+
+mongoose.connect(MONGO_URI, {useNewUrlParser: true})
+    .then(() => console.log('Connected to mongod server'))
+    .catch((e) => console.error(e));
+
+module.exports = conn;
