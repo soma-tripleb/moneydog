65cf541d759d4589d4eafe6ed475f01b27e5ca7c
jaeyeon kim <foodsksms@gmail.com>
2019-08-08 19:07:21 +0900

    #67 [subscription component] subscription component 만들기
    - Subscription Service, Repository, Model, Controller만들고 api테스트

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ const db = require('./db/mongoDB');
 const indexRouter = require('./routes/index');
 // const usersRouter = require('./routes/users');
 const usersRouter = require('./components/user/userController');
+const subscriptionRouter = require('./components/subscription/subscriptionController');
 
 const app = express();
 
@@ -23,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/subscriptions', subscriptionRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
