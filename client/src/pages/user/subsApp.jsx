c0b5179a53967e99e9ceb3a2ba1e80f61a78e744
parkyounghwan <dudrnxps1@gmail.com>
2019-08-08 16:49:59 +0900

    #65, 자식 Component 의 state 부모로 전달

--- client/src/pages/user/subsApp.jsx
+++ client/src/pages/user/subsApp.jsx
@@ -1,22 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
+
+import '../../static/style/component/user/subs-app.css';
 
 class SubsApp extends Component {
   state = {
     number: this.props.number,
     logo: this.props.logo,
-    title: this.props.title,
+    name: this.props.name,
+    label: this.props.label,
   };
 
-  handleClick = (e) => {
-    this.props.onInsert('testdata');
+  handleClick = () => {
+    const {subsAppInfo} = this.props;
+  
+    this.props.onInsert(subsAppInfo.number, subsAppInfo.logo, subsAppInfo.name);
   }
 
   render() {
     const { subsAppInfo } = this.props;
 
     return (
-      <div className="w-100 p-3" id="subs-inner-left-element" onClick={this.handleClick.bind(this)}>
-        <p>{subsAppInfo.number}&nbsp;&nbsp;{subsAppInfo.logo}&nbsp;&nbsp;{subsAppInfo.title}</p>
+      <div className="w-100 p-3" id="subs-inner-left-element">
+        <button onClick={this.handleClick.bind(this)}>
+          {subsAppInfo.label}
+        </button>
+        <p className="subs-app-info">{subsAppInfo.number}&nbsp;&nbsp;{subsAppInfo.logo}&nbsp;&nbsp;{subsAppInfo.name}</p>
       </div>
     )
   }
