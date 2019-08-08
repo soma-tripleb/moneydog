f81149dab7d79064ae9661d28734fac59322b742
jaeyeon kim <foodsksms@gmail.com>
2019-08-08 16:32:17 +0900

    #64 [mongoose] mongoose에서 users collection 조회
    - mongoose에 있는 user dummy 데이터 조회

--- server/components/user/userRepository.js
+++ server/components/user/userRepository.js
@@ -1,7 +1,11 @@
 const User = require('../../models/user');
 
 const getUserList = () => {
-  return User.find({});
+  console.log(User.find({}))
+  return User.find({}, (err, users) => {
+    if (err) throw err;
+    console.log('users : ',users);
+  });
 };
 
 const getUserById = (params) => {
@@ -9,7 +13,13 @@ const getUserById = (params) => {
   return User.find(modelParams);
 }
 
+const createUser = () => {
+  let user = {username : 'jimmy', password: '1234', content: 'helloworld'};
+  return User.create(user);
+}
+
 module.exports = {
   getUserList: getUserList,
   getUserById: getUserById,
+  createUser: createUser,
 }
