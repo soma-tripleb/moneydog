f613217edc860c53d92266ca70ded2fb6600f7ab
JaeyeonKIM <foodsksms@gmail.com>
2019-08-12 00:04:24 +0900

    #[jy-issue-7] google parser만드는중
    - GET /gmail/messages/get 요청에 대한 응답을 미리 넣어둠
    - response에 대한 응답을 base64로 디코딩, html형태로 만들어주고, googleReceipt에 대한 양식을 googleReceipt.html파일로 임시 저장.
    - google receipt에 대한 파서 만드는중

--- server/components/subscription/subscriptionService.js
+++ server/components/subscription/subscriptionService.js
@@ -1,4 +1,7 @@
 const subscriptionRepository = require('./subscriptionRepository');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const DomParser = require('dom-parser');
 
 const getSubscriptionList = () => {
   return subscriptionRepository.getSubscriptionList();
@@ -8,7 +11,47 @@ const getSubscriptionByName = (name) => {
   return subscriptionRepository.getSubscriptionByName(name);
 }
 
+const base64ToUtf8 = (base64encoded) => {
+  return Buffer.from(base64encoded, 'base64').toString('utf8');
+}
+
+const convertHtml = (rawHtml) => {
+  const parser = new DomParser();
+  const wrapper = parser.parseFromString(rawHtml, 'text/html');
+  return wrapper.rawHTML;
+}
+
+const getAppleInfo = (rawHtml) => {
+  const $ = cheerio.load(convertHtml(rawHtml));
+  service = {};
+  service.email = $('body > table:nth-child(4) > tbody > tr > td > div.aapl-desktop-div > table > tbody > tr:nth-child(4) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(1) > td:nth-child(1)').text().split('ID')[1];
+  service.name = $('span[class=title]').contents().get('0').data;
+  service.date = $('body > table:nth-child(4) > tbody > tr > td > div.aapl-desktop-div > table > tbody > tr:nth-child(4) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(2) > td').text().split('날짜')[1];
+  service.duration = $('span[class=duration]').contents().get('0').data;
+  service.renewal = $('span[class=renewal]').contents().get('0').data.trim();
+  return service;
+}
+
+const getGoogleInfo = (rawHtml) => {
+  const $ = cheerio.load(convertHtml(rawHtml));
+  service = {};
+  let name = $('#gamma > div > div:nth-child(2) > div > div:nth-child(6) > table:nth-child(5) > tbody > tr:nth-child(2) > td:nth-child(1) > span > span').text().trim();
+  let renewal = $('#gamma > div > div:nth-child(2) > div > div:nth-child(6) > table:nth-child(5) > tbody > tr:nth-child(3) > td:nth-child(1)').text().split(':')[1].trim();
+  let price = $('#gamma > div > div:nth-child(2) > div > div:nth-child(6) > table:nth-child(5) > tbody > tr:nth-child(2) > td:nth-child(2) > span > span').text();
+  let date = $('#gamma > div > div:nth-child(2) > div > div:nth-child(5)');
+  console.log(name);
+  // console.log(date);
+  console.log(renewal);
+  console.log(price);
+}
+
 module.exports = {
   getSubscriptionList: getSubscriptionList,
   getSubscriptionByName: getSubscriptionByName,
-}
+};
+
+const buf = JSON.parse(fs.readFileSync('./response.json'));
+
+const base = buf.payload.parts[1].body.data;
+
+getGoogleInfo(base64ToUtf8(base));
