93165e29150c3950b4dacb4095142f3094039a7c
jaeyeon kim <foodsksms@gmail.com>
2019-08-29 21:45:27 +0900

    [jy-issue-30] Categories안에 sortByPrice, sortByLeftDay로 정렬 만들기
    - state관리 이슈가 발생. sort를 하면 원본객체를 바꾸기 때문에. Object.sort를 이용.
    - bomx? redux를 써야된다...

--- client/src/pages/dashboard/Categories.jsx
+++ client/src/pages/dashboard/Categories.jsx
@@ -9,7 +9,7 @@ import './Categories.css';
 
 class Categories extends Component {
   state = {
-
+    subscriptions: null,
   };
 
   arr = {
@@ -25,24 +25,29 @@ class Categories extends Component {
   };
 
   sortByPrice = (subscriptions) => {
-    console.log('clicked sortby price');
-    subscriptions.sort((a, b) => (a.price > b.price) ? -1: 1);
+    // subscriptions.sort((a, b) => (a.price > b.price) ? -1: 1);
+    const sortedSubscriptions = Object.assign([], subscriptions).sort((a, b) => (a.price > b.price) ? -1 : 1);
+    this.setState({subscriptions: sortedSubscriptions}, () => {
+      console.log(this.state)
+    });
   };
 
   sortByLeftDay = (subscriptions) => {
-    console.log('clicked sortby leftday');
-    subscriptions.sort((a, b) => (this.calLeftDay(a) > this.calLeftDay(b) ? -1 : 1));
-    console.log(subscriptions);
+    const sortedSubscriptions = Object.assign([], subscriptions).sort((a, b) => (this.calLeftDay(a) > this.calLeftDay(b)) ? -1 : 1);
+    this.setState({subscriptions: sortedSubscriptions}, () => {
+      console.log(this.state)
+    });
   };
 
   render() {
-    //TODO :: ajax 로 data를 받아와서 처음에 null 값 error 가뜸 이곳에 스피너 같은거 넢어 주어야함
+
+    const {data} = this.props;
+
     if (this.props.data == null) {
-      return (
-          <>
-          </>
-      )
+      return null;
     }
+    //TODO :: ajax 로 data를 받아와서 처음에 null 값 error 가뜸 이곳에 스피너 같은거 넢어 주어야함
+    // console.log(this.props.data.subscriptions);
 
     return (
         <div>
@@ -50,17 +55,17 @@ class Categories extends Component {
           <p><u> 구독 중인 서비스 </u></p>
           <PageHeader title="구독 중인 서비스"
                       extra={[
-                        <Button key="2" onClick={this.sortByPrice(this.props.data.subscriptions)}>
+                        <Button key="2" onClick={() => {this.sortByPrice(data.subscriptions)}}>
                           가격 순
                         </Button>,
-                        <Button key="1" type="primary" onClick={this.sortByLeftDay(this.props.data.subscriptions)}>
+                        <Button key="1" onClick={() => {this.sortByLeftDay(data.subscriptions)}}>
                           남은 일
                         </Button>,
                       ]}>
           </PageHeader>
           <br/>
           <div>
-            {this.props.data.subscriptions.map((data, index) => {
+            {data.subscriptions.map((data, index) => {
               return (
                   <div key={index} className="container w-100 p-3" id="inner-element">
                     <div className="row">
--- client/src/pages/dashboard/test.js
+++
@@ -1,6 +0,0 @@
-const calDate = (renewal) => {
-  const result = new Date(renewal) - new Date(new Date().toISOString().slice(0, 10));
-  return result;
-}
-const temp = calDate("2019-09-25");
-console.log(temp);
