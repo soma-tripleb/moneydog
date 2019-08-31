cf6f78ba9f4c067184697303b03925c79c9c5e4e
YeonBot <tjdds1109@naver.com>
2019-08-31 14:39:24 +0900

    #24 로그인 JWT 반환 코드 작성
    - node 에 jsonwebtoken 모듈 설치
    - sha256 을 이용해 jwt 생성 후 반환

--- server/components/user/userService.js
+++ server/components/user/userService.js
@@ -1,4 +1,9 @@
+require('dotenv').config();
+
 const UserRepository = require('./userRepository');
+
+const jwt = require('jsonwebtoken');
+const secretCode = `${process.env.JWT_SECRET}`;
 
 const register = async (userInfo) => {
   const user = await UserRepository.getUserByEmail(userInfo.email);
@@ -18,9 +23,20 @@ const login = async (userInfo) => {
     return 200;
   } else if (user.password !== userInfo.password) {
     return 409;
-  };
+  }
 };
 
+const createJWT = (email) => {
+  const token = jwt.sign({
+    email: email,
+  },
+  secretCode,
+  {
+    expiresIn:
+  '5m',
+  });
+  return token;
+};
 
 const getUserByEmail = (email) => {
   // return UserRepository.getUserByEmail(email);
@@ -30,5 +46,6 @@ const getUserByEmail = (email) => {
 module.exports = {
   register: register,
   login: login,
+  createJWT: createJWT,
   getUserByEmail: getUserByEmail,
-}
+};
