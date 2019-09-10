31375f0f0e09e6e5dabf091b8941ff401f29dd3c
jaeyeon kim <foodsksms@gmail.com>
2019-09-10 21:59:06 +0900

    [jy-issue-48] express 실행환경 분리
    - cross-env NODE_ENV=test or production 명령어를 추가해서 test모드로 분리
    - package.json에 test cross-env NODE_ENV=test를 통해 test환경에서는 mockgoose를 사용

--- server/src/dbConfig/mongoDB.js
+++ server/src/dbConfig/mongoDB.js
@@ -8,6 +8,7 @@ const conn = mongoose.createConnection(MONGO_URI);
 
 // connect 하는 부분
 const mongoConnect = () => {
+  console.log(`실행모드 : ${process.env.NODE_ENV}`);
   if (process.env.NODE_ENV === 'test') {
     const Mockgoose = require('mockgoose').Mockgoose;
     const mockgoose = new Mockgoose(mongoose);
