cafba2ed6ce97d3ca1281803fd02fa1c24bf1a42
JaeyeonKIM <foodsksms@gmail.com>
2019-09-12 01:13:42 +0900

    #48 userRepository create테스트
    - 이전에는 UserModel에 직접적으로 create를 했지만, userRepository에 있는 createuser메서드를 통해 create를 구현했다.

--- server/test/repository/UserRepotiroty.spec.js
+++ server/test/repository/UserRepotiroty.spec.js
@@ -41,13 +41,26 @@ after(async () => {
 describe('#UserRepository Test', () => {
   it('#create', async () => {
     const createUser = {
-
-    }
-    const user = await userRepository.createUser(UserMock);
-    expect(user.email).to.equal('test@test.com');
+      email: 'jimmy@naver.com',
+      password: '1234',
+      nickname: 'jimmy',
+      salt: 111,
+      role: 'user',
+      subscription: {
+        name: 'netflix',
+        price: 14000,
+        channel: 'ios',
+        pricePlan: {
+          title: 'premium',
+          price: 14000,
+        },
+      },
+    };
+    const user = await userRepository.createUser(createUser);
+    expect(user.email).to.equal('jimmy@naver.com');
     expect(user.password).to.equal('1234');
     const subscription = user.subscription;
-    expect(subscription.name).to.equal('test-title');
+    expect(subscription.name).to.equal('netflix');
   });
 
   it('#read user', async () => {
