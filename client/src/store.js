4c78a07470ca63fb3a15af50b250dbdfc4c46244
jaeyeon kim <foodsksms@gmail.com>
2019-09-19 22:59:50 +0900

    #79 redux-persist 적용 시작.
    - redux-persist tutorial대로 store생성하는 부분 변경

--- client/src/store.js
+++ client/src/store.js
@@ -3,16 +3,31 @@ import {composeWithDevTools} from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import {createLogger} from 'redux-logger';
 import rootReducer from './reducers';
+import {persistStore, persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const initialState = {};
 const logger = createLogger();
 
 const middleware = [thunk, logger];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
+const persistConfig = {
+  key: 'root',
+  storage,
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+//
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   composeWithDevTools(applyMiddleware(...middleware)),
+// );
+//
+// export default store;
+
+export default () => {
+  const store = createStore(persistReducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
+  const persistor = persistStore(store);
+  return {store, persistor};
+};
