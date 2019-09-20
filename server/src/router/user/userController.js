c2ac2e4e8817c17827051882e8e3bd98b44bb889
jaeyeon kim <foodsksms@gmail.com>
2019-09-20 17:06:08 +0900

    #79 /users/auth/check 설계중
    - auth를 기반으로 user조회를 할 예정

--- server/src/router/user/userController.js
+++ server/src/router/user/userController.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import UserService from './userService';
+import jwtDecode from 'jwt-decode';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  console.log('GET /users/ called');
   UserService.getUserList()
     .then((result) => {
       res.send(result);
@@ -15,6 +17,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:email', (req, res) => {
+  console.log(`/users/email 찍힘 ${req.param.email}`)
   UserService.getUser(req.params.email)
     .then((result) => {
       res.send(result);
@@ -27,6 +30,7 @@ router.get('/:email', (req, res) => {
 
 // body: Json Data
 router.post('/', (req, res) => {
+  console.log('POST /users/ called');
   UserService.createOne(req.body)
     .then((result) => {
       res.send(result);
@@ -43,6 +47,21 @@ router.delete('/email/:email', (req, res) => {
     })
     .catch((err) => {
       res.send(err);
+    });
+});
+
+router.get('/auth/check', (req, res, next) => {
+  console.log('users/auth 실행');
+  console.log(`param ${req.param}`);
+  const userEmail = jwtDecode(req.param.token).param;
+  console.log(`userEmail : ${userEmail}`);
+  const user = userService.getUser(userEmail);
+  user
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.send('유저가 없습니다. ', err);
     });
 });
 
