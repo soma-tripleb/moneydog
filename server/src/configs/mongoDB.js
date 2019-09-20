7493fa0d35e20781a95df940b0e5a6f4d1e0db8d
YeonBot <tjdds1109@naver.com>
2019-09-20 15:52:57 +0900

    - test db 환경 분리 성공 !
    - 인메모리 mongo db로 테스트 환경 이전

--- server/src/configs/mongoDB.js
+++ server/src/configs/mongoDB.js
@@ -1,21 +1,41 @@
 require('dotenv').config();
 
+import 'babel-polyfill';
 import mongoose from 'mongoose';
-const { MongoMemoryServer } = require('mongodb-memory-server');
+import { MongoMemoryServer } from 'mongodb-memory-server';
 
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 
 const MONGO_URI = `${process.env.DB_SCHEMA}${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}`;
 
-const conn = mongoose.createConnection(MONGO_URI);
+const conn = mongoose.createConnection();
+
+let mongod = null;
+if (process.env.NODE_ENV === 'test') {
+  mongod = new MongoMemoryServer();
+}
+
+const createConn = async ()=>{
+  if (process.env.NODE_ENV === 'test') {
+    const uri = await mongod.getConnectionString()
+      .then((ur) => ur);
+
+    conn.openUri(uri);
+  } else {
+    conn.openUri(MONGO_URI);
+  }
+};
+
+createConn();
+
 
 // connect 하는 부분
 const mongoConnect = () => {
   mongoose.set('useFindAndModify', false);
   mongoose.set('useNewUrlParser', true);
   if (process.env.NODE_ENV === 'test') {
-    new MongoMemoryServer().getConnectionString()
+    mongod.getConnectionString()
       .then((url) =>{
         return mongoose.connect(url);
       })
@@ -33,11 +53,20 @@ const mongoConnect = () => {
 };
 
 const mongoDisConnect = () => {
-  mongoose.disconnect()
-    .then(() => console.log('Disconnected mongo server'))
-    .catch((e) => {
-      console.error(e);
-    });
+  if (process.env.NODE_ENV === 'test') {
+    mongoose.disconnect()
+      .then(() => console.log('Disconnected mongo server'))
+      .catch((e) => {
+        console.error(e);
+      });
+    mongod.stop();
+  } else {
+    mongoose.disconnect()
+      .then(() => console.log('Disconnected mongo server'))
+      .catch((e) => {
+        console.error(e);
+      });
+  }
 };
 
 export {conn, mongoConnect, mongoDisConnect};
