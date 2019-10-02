8c7dc11454de74b27ac4334cdfaff85fffc503b1
정성연 <tjdds1109@naver.com>
2019-10-02 14:32:58 +0900

    Merge branch 'dev' of https://git.swmgit.org/root/p1022_moneydog-2 into sy-issue-4
    사용자가 회원 가입으로 들어올 시 구독 정보를 추가하는 페이지로 이동
    사용자가 구독 정보를 추가 모두 추가하면 dashboard 로 이동

--- src/includes/Head.jsx
+++ src/includes/Head.jsx
@@ -16,12 +16,6 @@ class Head extends Component {
       return (
         <>
           <li className="nav-item">
-            <Link to="/user/subscribing" className="nav-link">Subscribing</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/user/subscribing-Info" className="nav-link">Subscribing Info</Link>
-          </li>
-          <li className="nav-item">
             <Link to="/user/dashboard" className="nav-link">Dashboard</Link>
           </li>
           <li className="nav-item">
@@ -31,6 +25,17 @@ class Head extends Component {
             <a onClick={this.logout} className="nav-link">Logout</a>
           </li>
         </>
+      );
+    } else if (this.props.status === 'ADD_SUBS_STEP'){
+      return (
+          <>
+            <li className="nav-item">
+              <Link to="/user/subscribing" className="nav-link">Subscribing</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/user/subscribing-Info" className="nav-link">Subscribing Info</Link>
+            </li>
+          </>
       );
     } else {
       return (
