68c77c992da0d44e7df5d2400f7ca87a36e4db2f
jaeyeon kim <foodsksms@gmail.com>
2019-10-02 15:42:01 +0900

    #3 netflix 해지 확인 - renewaldate(구독갱신일)과 expiredDate(해지예정일)에 대한 포맷이 같아서 Regex메서드명을 바꿈. - regex메서드들 이름을 바꿈 - 해지날짜를 업데이트, service.nextSubsrive Boolean type의 필드를 만듬으로서 다음달에도 해지여부에 대한 true, false로 판별

--- src/mailParser/netflix/netflixParser.js
+++ src/mailParser/netflix/netflixParser.js
@@ -9,13 +9,20 @@ const getSubject = (response) => {
 
 const checkStatus = (response) => {
   if (getSubject(response).contains('작별')) {
-    // update unsubsribe method
+    // update expiredDate and nextSubscribe true to false
   } else if (getSubject(response).contains('업데이트')) {
     // membership upgrade
   } else {
     // membership restart or using
   }
-}
+};
+
+const getExpiredDate = (response) => {
+  const jsonObject = commonParser.stringToJsonObject(commonParser.base64ToUtf8(response));
+  const dom = commonParser.convertHtml(commonParser.base64ToUtf8(jsonObject.payload.parts[1].body.data));
+  const $ = cheerio.load(dom);
+  const expiredDate = renewalAndExpiredRegex($('#container > tbody > tr > td > table.shell > tbody > tr > td > table > tbody > tr:nth-child(4) > td').text());
+};
 
 const getNetflixInfo = (response) => {
   const jsonObject = commonParser.stringToJsonObject(commonParser.base64ToUtf8(response));
@@ -23,28 +30,28 @@ const getNetflixInfo = (response) => {
   const $ = cheerio.load(dom);
   service = {};
   service.email = $('#container > tbody > tr > td > table.shell > tbody > tr > td > table > tbody > tr:nth-child(8) > td > table > tbody > tr:nth-child(2) > td').text();
-  service.name = convertNameReg($('#container > tbody > tr > td > table.shell > tbody > tr > td > table > tbody > tr:nth-child(11) > td > table > tbody > tr:nth-child(2) > td').text());
-  service.price = convertPriceReg($('#container > tbody > tr > td > table.shell > tbody > tr > td > table > tbody > tr:nth-child(11) > td > table > tbody > tr:nth-child(2) > td').text());
-  service.renewal = convertRenewalReg($('#container > tbody > tr > td > table.shell > tbody > tr > td > table > tbody > tr:nth-child(11) > td > table > tbody > tr:nth-child(1) > td').text().trim());
+  service.name = nameRegex($('#container > tbody > tr > td > table.shell > tbody > tr > td > table > tbody > tr:nth-child(11) > td > table > tbody > tr:nth-child(2) > td').text());
+  service.price = priceRegex($('#container > tbody > tr > td > table.shell > tbody > tr > td > table > tbody > tr:nth-child(11) > td > table > tbody > tr:nth-child(2) > td').text());
+  service.renewal = renewalAndExpiredRegex($('#container > tbody > tr > td > table.shell > tbody > tr > td > table > tbody > tr:nth-child(11) > td > table > tbody > tr:nth-child(1) > td').text().trim());
   service.periodMonth = 1;
   service.nextSubsribe = true;
   return service;
 };
 
 // extract service name
-const convertNameReg = (data) => {
+const nameRegex = (data) => {
   const regex = /[가-힣]{3,4}/;
   return regex.exec(data)[0];
 };
 
 // extract service price
-const convertPriceReg = (data) => {
+const priceRegex = (data) => {
   const priceRegex = /[0-9]{2},[0-9]{3}/;
   return parseInt(priceRegex.exec(data)[0].replace(',', ''));
 };
 
 // extract and calculate renewal date
-const convertRenewalReg = (data) => {
+const renewalAndExpiredRegex = (data) => {
   const yearRegex = /[0-9]{4}/;
   const monthRegex = /[0-9]{2}/g;
   const monthAndDay = data.match(monthRegex); // return 4 values [20,19,10,31] => 2019-10-31.
@@ -56,3 +63,4 @@ const fs = require('fs');
 const response = fs.readFileSync('./netflix_unsubscribe.json');
 
 console.log(getSubject(response));
+console.log(getExpiredDate(response));
