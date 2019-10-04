504ba4d434cd2ae076acd7504f187e8ed9a542e6
jaeyeon kim <foodsksms@gmail.com>
2019-10-04 11:18:53 +0900

    #6 watcha parser
    - 결제금액 price 명명
    - 결제금액 정규표현식으로 추출하는 메서드 구현.
    - watcha_renewal.html에 포함된 개인정보 다 제거

--- src/mailParser/watcha/watchaParser.js
+++ src/mailParser/watcha/watchaParser.js
@@ -7,10 +7,18 @@ const getWathcaInfo = (response) => {
   const $ = commonParser.getParser(response);
   service = {};
   service.paymentDate = paymentDateRegex($('body > div > div > div:nth-child(4) > div > div:nth-child(5) > div > div:nth-child(2) > p > span:nth-child(1)').text());
+  service.price = priceRegex($('body > div > div > div:nth-child(4) > div > div:nth-child(5) > div > div:nth-child(2) > p > span:nth-child(5)').text());
+  console.log(service);
 };
 
 const paymentDateRegex = (paymentDate) => {
   const regex = /[0-9]{3,4}-[0-9]{1,2}-[0-9]{1,2}/;
   return regex.exec(paymentDate)[0];
-}
+};
+
+const priceRegex = (price) => {
+  const regex = /[0-9]{1,2},[0-9]{3}/;
+  return parseInt(regex.exec(price)[0].replace(',', ''));
+};
+
 getWathcaInfo(response);
