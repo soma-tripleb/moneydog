3a1748a73b149cebb76265d777c7b521bc6d5f1d
jaeyeon kim <foodsksms@gmail.com>
2019-10-05 16:53:44 +0900

    # docuemnt db 서버에서 사용해보기
    - db환경변수 바꿔서 접속해보기

--- server/src/config/document.js
+++
@@ -1,7 +0,0 @@
-mongoose.connect(database.url, {
-  useNewUrlParser: true,
-  ssl: true,
-  sslValidate: false,
-  sslCA: fs.readFileSync('./rds-combined-ca-bundle.pem')})
-  .then(() => console.log('Connection to DB successful'))
-  .catch((err) => console.error(err,'Error'));
