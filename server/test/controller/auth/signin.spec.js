65c23834aa28d7d2e412758237b2e7eb837c2af5
정성연 <tjdds1109@naver.com>
2019-10-07 13:16:00 +0900

    log dir 변경

--- server/test/controller/auth/signin.spec.js
+++ server/test/controller/auth/signin.spec.js
@@ -1,4 +1,4 @@
-import 'babel-polyfill';
+import '@babel/polyfill';
 
 import request from 'supertest';
 import app from '../../../app';
--- server/test/controller/index.spec.js
+++ server/test/controller/index.spec.js
@@ -1,4 +1,4 @@
-import 'babel-polyfill';
+import '@babel/polyfill';
 
 import assert from 'assert';
 import request from 'supertest';
