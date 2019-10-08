8577cb9d0e3cbc75710e9f12181eb50c2f188fee
JaeyeonKIM <foodsksms@gmail.com>
2019-10-09 00:12:22 +0900

    #87 SubsTmplService.spec.js Test코드 수정중
    - findAll()의 결과에 대한 assertion추가.

--- server/test/service/subscriptionTemplate/subsTmplService.spec.js
+++ server/test/service/subscriptionTemplate/subsTmplService.spec.js
@@ -5,11 +5,15 @@ import {assert} from 'chai';
 
 describe('SubscriptionTemplate Service Test', () => {
   before(() => {
+    console.log('before connection');
     mongoConnect();
+    console.log('after connection');
   });
 
   after(() => {
+    console.log('before disconnect');
     mongoDisConnect();
+    console.log('after disconnect');
   });
 
   describe('Repository Method', () => {
@@ -27,38 +31,41 @@ describe('SubscriptionTemplate Service Test', () => {
     //   });
     // });
 
-    describe('#saveOne()', () => {
-      it('SubsTmpl 저장하기', (done) => {
-        SubsTmplRepo.saveOne(SubsTmplMock)
-          .then((result) => {
-            console.log(result);
-            done();
-          })
-          .catch((err) => {
-            done(err);
-          });
-      });
-    });
-
-    describe('findOne()', () => {
-      it('SubsTmpl 이름으로 구독 서비스 정보 찾기', (done) => {
-        const SubsTmplMockName = SubsTmplMock.name;
-        SubsTmplRepo.findByName(SubsTmplMockName)
-          .then((result) => {
-            console.log(result);
-            done();
-          })
-          .catch((err) => {
-            done(err);
-          });
-      });
-    });
+    // describe('#saveOne()', () => {
+    //   it('SubsTmpl 저장하기', (done) => {
+    //     console.log('save db');
+    //     SubsTmplRepo.saveOne(SubsTmplMock.userInputList[0])
+    //       .then((result) => {
+    //         console.log(result);
+    //         done();
+    //       })
+    //       .catch((err) => {
+    //         done(err);
+    //       });
+    //   });
+    // });
+    //
+    // describe('findOne()', () => {
+    //   it('SubsTmpl 이름으로 구독 서비스 정보 찾기', (done) => {
+    //     const SubsTmplMockName = SubsTmplMock.userInputList[0].name;
+    //     SubsTmplRepo.findByName(SubsTmplMockName)
+    //       .then((result) => {
+    //         console.log(result);
+    //         done();
+    //       })
+    //       .catch((err) => {
+    //         done(err);
+    //       });
+    //   });
+    // });
 
     describe('findAll()', () => {
       it('SubsTmpl 전체 구독 서비스 목록 출력', (done) => {
         SubsTmplRepo.findAll()
           .then((result) => {
-            assert.equal(8, result.length);
+            const list = result.message;
+            assert.equal(8, list.length);
+            assert.equal('Flo', list[0].name);
             done();
           })
           .catch((err) => {
