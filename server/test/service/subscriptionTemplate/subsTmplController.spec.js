faa7c043af1699c043faeb8da31770e27ef04122
jaeyeon kim <foodsksms@gmail.com>
2019-10-09 15:19:56 +0900

    # SubscriptionTemplateController 테스트코드 작성
    - package.json 명령어 수정. supertest의 경우 cross-env명령어가 안먹는다. 우선은 cross-env명령어를 제거하고 테스트코드를 실행시켰다.

--- server/test/service/subscriptionTemplate/subsTmplController.spec.js
+++ server/test/service/subscriptionTemplate/subsTmplController.spec.js
@@ -1,5 +1,6 @@
 import '@babel/polyfill';
 import request from 'supertest';
+import {mongoConnect, mongoDisConnect} from '../../../src/config/mongoDB';
 import app from '../../../app';
 
 describe('Subscription Template Controller Test', () => {
@@ -10,5 +11,9 @@ describe('Subscription Template Controller Test', () => {
         if (err) throw err;
         console.log(JSON.stringify(res));
       });
+  });
+  after((done) => {
+    mongoDisConnect();
+    done();
   });
 });
