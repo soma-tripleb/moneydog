ffb03cd404cf078b43fe2fe37b434856b6956d52
jaeyeon kim <foodsksms@gmail.com>
2019-10-09 15:24:25 +0900

    # SubscriptionTemplateController 테스트코드 작성
    - 요청을 보내기전에 before를 이용해서 token을 가져오기.

--- server/test/service/subscriptionTemplate/subsTmplController.spec.js
+++ server/test/service/subscriptionTemplate/subsTmplController.spec.js
@@ -4,6 +4,22 @@ import {mongoConnect, mongoDisConnect} from '../../../src/config/mongoDB';
 import app from '../../../app';
 
 describe('Subscription Template Controller Test', () => {
+  before((done) => {
+    request(app)
+      .post('/auth/signIn')
+      .send({
+        userInfo: {
+          email: 'admin@fkii.org',
+          password: '1234',
+        },
+      })
+      .end((err, res) => {
+        if (err) done(err);
+        const token = res.text.token;
+        console.log(`token : ${token}`);
+        done();
+      });
+  });
   it('GET /subs-tmpl 테스트하기', () => {
     request(app)
       .get('/subs-tmpl')
