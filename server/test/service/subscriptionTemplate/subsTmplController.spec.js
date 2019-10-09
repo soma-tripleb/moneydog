6db84617aa98b9591f955425d34d086e7bb9c7e0
jaeyeon kim <foodsksms@gmail.com>
2019-10-09 15:57:25 +0900

    # before작성중
    - before에서 token유무를 검증하는 assertion추가. assert.isDefined로 토큰이 undefined인지 아닌지를 체크한다.

--- server/test/service/subscriptionTemplate/subsTmplController.spec.js
+++ server/test/service/subscriptionTemplate/subsTmplController.spec.js
@@ -1,26 +1,25 @@
 import '@babel/polyfill';
 import request from 'supertest';
+import {assert} from 'chai';
 import {mongoConnect, mongoDisConnect} from '../../../src/config/mongoDB';
 import app from '../../../app';
 
 describe('Subscription Template Controller Test', () => {
+  let token;
   before((done) => {
     request(app)
       .post('/auth/signIn')
-      .send({
-        userInfo: {
-          email: 'admin@fkii.org',
-          password: '1234',
-        },
-      }
+      .send({userInfo: {email: 'admin@fkii.org', password: '1234'}})
       .end((err, res) => {
         if (err) done(err);
-        const json = JSON.stringify(res);
-        const token = json.token;
+        const json = JSON.parse(res.text);
+        token = json.token;
+        assert.isDefined(token, 'token is defined');
         done();
-      })
+      });
   });
   it('GET /subs-tmpl 테스트하기', () => {
+    console.log(`it token : ${token}`);
     request(app)
       .get('/subs-tmpl')
       .end((err, res) => {
