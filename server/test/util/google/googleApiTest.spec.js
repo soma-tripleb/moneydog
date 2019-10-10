da7c269d6472ad99c251b47f0dd6cc648d6acde8
jaeyeon kim <foodsksms@gmail.com>
2019-10-10 18:10:14 +0900

    #88 test충돌 해결.
    - googleApiTest.spec.js충돌해결

--- server/test/util/google/googleApiTest.spec.js
+++ server/test/util/google/googleApiTest.spec.js
@@ -6,9 +6,7 @@ describe('Google API', () => {
 
   describe('OAuth', () => {
     it('site info check', (done) => {
-      assert.equal(process.env.GOOGLE_API_OAUTH_URL, 'https://accounts.google.com/o/oauth2/v2/auth');
       assert.equal(process.env.GOOGLE_API_REDIRECT_URL, 'http://localhost:5000/google/signup');
-      assert.equal(process.env.GOOGLE_API_REDIRECT_URL_ENCODE, 'http%3A%2F%2Flocalhost%3A5000%2Fgoogle%2Fsignup');
       assert.equal(process.env.GOOGLE_API_CLIENT_ID, '532345922072-50gar7lh5ca5rvepjs7iisa6lu28d741.apps.googleusercontent.com');
       assert.equal(process.env.GOOGLE_API_CLIENT_SECRET, 'QNzzHMDXr4MXhpxUuefGZnXO');
       done();
