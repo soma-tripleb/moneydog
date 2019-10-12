d5002bc9d54d287a4d9bf01326625f287db358fd
parkyounghwan <dudrnxps1@gmail.com>
2019-10-12 20:27:24 +0900

    #dev, 카카오 친구 추가 버튼만 삽입 해봄

--- client/src/pages/info/info.jsx
+++ client/src/pages/info/info.jsx
@@ -1,17 +1,17 @@
-import React, {Component} from 'react';
-import {Row, Col, Input, Layout, Form, DatePicker, TimePicker, Select, Button, Avatar} from 'antd';
+import React, { Component } from 'react';
+import { Row, Col, Input, Layout, Form, DatePicker, TimePicker, Select, Button, Avatar } from 'antd';
 
-const {Header, Footer, Content} = Layout;
-const {Option} = Select;
+const { Header, Footer, Content } = Layout;
+const { Option } = Select;
 
 const formItemLayout = {
   labelCol: {
-    xs: {span: 24},
-    sm: {span: 12},
+    xs: { span: 24 },
+    sm: { span: 12 },
   },
   wrapperCol: {
-    xs: {span: 24},
-    sm: {span: 12},
+    xs: { span: 24 },
+    sm: { span: 12 },
   },
 };
 
@@ -22,29 +22,40 @@ class Info extends Component {
     text: 'Info Page',
   };
 
+  componentDidMount() {
+    const script = document.createElement('script');
+
+    script.src = '//developers.kakao.com/sdk/js/kakao.min.js';
+    script.async = true;
+
+    document.body.appendChild(script);
+    console.log(script);
+  };
+
   render() {
     return (
       <>
         <div>
           <h1>{this.state.text}</h1>
         </div>
+
         <Row>
-          <div style={{padding: 24, background: '#fff', minHeight: 360}}>
+          <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
             <Layout>
               <Header>
-                <h1 style={{lineHeight: '64px', color: '#fff'}}></h1>
+                <h1 style={{ lineHeight: '64px', color: '#fff' }}></h1>
               </Header>
 
-              <Content style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+              <Content style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                 <Avatar icon='user' shape='square' style={{
                   width: 100,
                   height: 100,
                   display: 'flex',
                   flexDirection: 'column',
                   justifyContent: 'center',
-                }}/>
+                }} />
                 <Input size='large' placeholder='Enter Price'
-                  style={{width: '33%', margin: 'auto', marginTop: '1rem'}}/>
+                  style={{ width: '33%', margin: 'auto', marginTop: '1rem' }} />
                 <Form {...formItemLayout}>
                   <div className='form-flex-container' style={{
                     padding: '1rem',
@@ -54,18 +65,18 @@ class Info extends Component {
                     alignItems: 'center',
                   }}>
                     <Col span={12}>
-                      <Form.Item label='Name'/>
-                      <Form.Item label='Description'/>
-                      <Form.Item label='First bill'/>
-                      <Form.Item label='Remind me'/>
-                      <Form.Item label='Currency'/>
+                      <Form.Item label='Name' />
+                      <Form.Item label='Description' />
+                      <Form.Item label='First bill' />
+                      <Form.Item label='Remind me' />
+                      <Form.Item label='Currency' />
                     </Col>
                     <Col span={12}>
-                      <Input placeholder="Enter name" id="name"/>
-                      <Input placeholder="Enter description" id="warning"/>
-                      <DatePicker style={{width: '100%'}}/>
-                      <Input placeholder="I'm the content" id="success"/>
-                      <TimePicker style={{width: '100%'}}/>
+                      <Input placeholder="Enter name" id="name" />
+                      <Input placeholder="Enter description" id="warning" />
+                      <DatePicker style={{ width: '100%' }} />
+                      <Input placeholder="I'm the content" id="success" />
+                      <TimePicker style={{ width: '100%' }} />
                       <Select defaultValue="1">
                         <Option value="1">￦</Option>
                         <Option value="2">$</Option>
@@ -74,6 +85,11 @@ class Info extends Component {
                   </div>
                 </Form>
               </Content>
+
+              <a id="kakao-login-btn">
+                <img id="kakao-login-btn" src="https://kauth.kakao.com/public/widget/login/kr/kr_02_medium.png" style={{ cursor: 'pointer' }} onMouseOver="this.src='https://kauth.kakao.com/public/widget/login/kr/kr_02_medium_press.png'" onMouseOut="this.src='https://kauth.kakao.com/public/widget/login/kr/kr_02_medium.png'" />
+              </a>
+
               <Footer>
                 <Button type="primary" icon="plus" size='large'>Add</Button>
               </Footer>
