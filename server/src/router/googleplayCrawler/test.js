f8dcaae843a0636e3e4ac56847df0af313c5255a
jaeyeon kim <foodsksms@gmail.com>
2019-10-24 20:28:30 +0900

    querySelectAll구현중

--- server/src/router/googleplayCrawler/test.js
+++ server/src/router/googleplayCrawler/test.js
@@ -2,12 +2,11 @@ const puppeteer = require('puppeteer');
 
 const run = async () => {
   try {
-    // const browser = await puppeteer.launch({headless: false});
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('https://play.google.com/store/account/subscriptions');
     await page.waitFor('body');
-    await login(page, 'testid', 'testpassword');
+    await login(page, 'testID', 'testPWD');
     await browser.close();
   } catch (e) {
     console.log(`error : ${e}`);
@@ -21,6 +20,12 @@ const login = async (page, id, password) => {
   await page.$eval('#signIn', (e) => e.click());
   await page.waitFor(1000);
   await page.screenshot({path: 'testshot1.png'});
+  const list = await page.evaluate(() => {
+    console.log(document.querySelectorAll('#fcxH9b > div.WpDbMd > c-wiz:nth-child(4) > div > c-wiz > table > tbody > tr'));
+    return Array.from(document.querySelectorAll('#fcxH9b > div.WpDbMd > c-wiz:nth-child(4) > div > c-wiz > table > tbody > tr'));
+    // console.log(`list : ${list}`);
+  });
+  console.log(`list : ${list}`);
   return page;
 };
 
