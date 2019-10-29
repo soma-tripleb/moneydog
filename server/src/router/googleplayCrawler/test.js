730a4f504388404db3bad9f5754c213c360ff755
jaeyeon kim <foodsksms@gmail.com>
2019-10-29 19:17:00 +0900

    #107 puppeteer headless true로 사용
    - 내구독에서 내 주문내역으로 크롤링 주소를 변경했다.

--- server/src/router/googleplayCrawler/test.js
+++ server/src/router/googleplayCrawler/test.js
@@ -2,17 +2,14 @@ const puppeteer = require('puppeteer');
 
 const run = async () => {
   try {
-    // const browser = await puppeteer.launch();
-    const browser = await puppeteer.launch({
-      headless: false,
-    });
+    const browser = await puppeteer.launch();
+    // const browser = await puppeteer.launch({
+    //   headless: false,
+    // });
     const page = await browser.newPage();
-    console.log('page생성');
-    // await page.goto('https://play.google.com/store/account/subscriptions');
-    // await page.waitFor('body');
-    Promise.all([page.goto('https://play.google.com/store/account/subscriptions'), page.waitFor('body')])
+    Promise.all([page.goto('https://play.google.com/store/account/orderhistory'), page.waitFor('body')])
     console.log('page 이동');
-    await login(page, 'test', 'test');
+    await login(page, 'tet', 'password');
     await browser.close();
     console.log('browser close');
   } catch (e) {
@@ -25,21 +22,21 @@ const login = async (page, id, password) => {
     console.log('login page');
     await page.waitForNavigation();
     // await page.screenshot({path: 'testshot1.png'});
-    console.log('waitfornavagation');
     await page.type('#Email', id);
     await page.$eval('#next', (e) => e.click());
     await page.type('#Passwd', password);
     await page.$eval('#signIn', (e) => e.click());
-    await page.waitFor(1000);
     console.log('login success');
     // await page.screenshot({path: 'testshot2.png'});
     const list = await page.evaluate(() => {
-      console.log(document.querySelectorAll('#fcxH9b > div.WpDbMd > c-wiz:nth-child(4) > div > c-wiz > table > tbody > tr'));
+      console.log(document.querySelectorAll('#fcxH9b > div.WpDbMd > c-wiz:nth-child(7) > div > div.oOodye > table > tbody'));
       return Array.from(document.querySelectorAll('#fcxH9b > div.WpDbMd > c-wiz:nth-child(4) > div > c-wiz > table > tbody > tr'));
     });
-    return page;
+    console.log(list);
+    return list;
   } catch (e) {
     console.log(`login function error : ${e}`);
   }
 };
+
 run();
