9a3aa2609b6506a807db914422928ce9ea8a8de3
jaeyeon kim <foodsksms@gmail.com>
2019-10-29 16:34:49 +0900

    #107 puppeteer headless false error
    - puppeteer headless false 옵션으로 해두면 에러가 발생한다. 원인을 계속 찾고있다.

--- server/src/router/googleplayCrawler/test.js
+++ server/src/router/googleplayCrawler/test.js
@@ -2,31 +2,44 @@ const puppeteer = require('puppeteer');
 
 const run = async () => {
   try {
-    const browser = await puppeteer.launch();
+    // const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      headless: false,
+    });
     const page = await browser.newPage();
-    await page.goto('https://play.google.com/store/account/subscriptions');
-    await page.waitFor('body');
-    await login(page, 'testID', 'testPWD');
+    console.log('page생성');
+    // await page.goto('https://play.google.com/store/account/subscriptions');
+    // await page.waitFor('body');
+    Promise.all([page.goto('https://play.google.com/store/account/subscriptions'), page.waitFor('body')])
+    console.log('page 이동');
+    await login(page, 'test', 'test');
     await browser.close();
+    console.log('browser close');
   } catch (e) {
-    console.log(`error : ${e}`);
+    console.log(`${e}`);
   }
 };
 
 const login = async (page, id, password) => {
-  await page.type('#Email', id);
-  await page.$eval('#next', (e) => e.click());
-  await page.type('#Passwd', password);
-  await page.$eval('#signIn', (e) => e.click());
-  await page.waitFor(1000);
-  await page.screenshot({path: 'testshot1.png'});
-  const list = await page.evaluate(() => {
-    console.log(document.querySelectorAll('#fcxH9b > div.WpDbMd > c-wiz:nth-child(4) > div > c-wiz > table > tbody > tr'));
-    return Array.from(document.querySelectorAll('#fcxH9b > div.WpDbMd > c-wiz:nth-child(4) > div > c-wiz > table > tbody > tr'));
-    // console.log(`list : ${list}`);
-  });
-  console.log(`list : ${list}`);
-  return page;
+  try {
+    console.log('login page');
+    await page.waitForNavigation();
+    // await page.screenshot({path: 'testshot1.png'});
+    console.log('waitfornavagation');
+    await page.type('#Email', id);
+    await page.$eval('#next', (e) => e.click());
+    await page.type('#Passwd', password);
+    await page.$eval('#signIn', (e) => e.click());
+    await page.waitFor(1000);
+    console.log('login success');
+    // await page.screenshot({path: 'testshot2.png'});
+    const list = await page.evaluate(() => {
+      console.log(document.querySelectorAll('#fcxH9b > div.WpDbMd > c-wiz:nth-child(4) > div > c-wiz > table > tbody > tr'));
+      return Array.from(document.querySelectorAll('#fcxH9b > div.WpDbMd > c-wiz:nth-child(4) > div > c-wiz > table > tbody > tr'));
+    });
+    return page;
+  } catch (e) {
+    console.log(`login function error : ${e}`);
+  }
 };
-
 run();
