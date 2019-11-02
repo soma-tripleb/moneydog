48463f6faff8c7b894afc8ecba3b82d5a05fd7a7
JaeyeonKIM <foodsksms@gmail.com>
2019-11-02 19:27:25 +0900

    #107 google id입력
    - google id입력은 된다. 아직 password는 시차때문에 안된다.

--- server/src/router/googleplayCrawler/selenium.js
+++ server/src/router/googleplayCrawler/selenium.js
@@ -1,18 +1,21 @@
-// const webdriver = require('selenium-webdriver');
-// const By = webdriver.By;
-// const chrome = require('selenium-webdriver/chrome');
-// const driver = new webdriver.Builder().forBrowser('chrome').build();
-//
-// driver.get('https://www.google.com');
-const {Builder, By, Key, until} = require('selenium-webdriver');
+const webdriver = require('selenium-webdriver');
+const By = webdriver.By;
+const chrome = require('selenium-webdriver/chrome');
+const path = require('chromedriver').path;
 
-(async function example() {
-  let driver = await new Builder().forBrowser('firefox').build();
-  try {
-    await driver.get('http://www.google.com/ncr');
-    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-  } finally {
-    await driver.quit();
-  }
-})();
+const service = new chrome.ServiceBuilder(path).build();
+chrome.setDefaultService(service);
+
+const drvier = new webdriver.Builder()
+  .withCapabilities(webdriver.Capabilities.chrome())
+  .build();
+
+drvier.get('https://play.google.com/store/account/orderhistory');
+drvier.findElement(By.css('#identifierId')).sendKeys('moneydogtest1');
+drvier.findElement(By.css('#identifierNext')).click();
+drvier.findElement(By.css('#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input')).sendKeys('moneydog1234');
+drvier.findElement(By.css('#passwordNext')).click();
+// email input #identifierId
+// next button #identifierNext
+// password #password > div.aCsJod.oJeWuf > div > div.Xb9hP > input
+// pwd next button #passwordNext
