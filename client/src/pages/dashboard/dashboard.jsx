bc5fa6446dbec93bc62ed59b01f69a2556c7d0ef
정성연 <tjdds1109@naver.com>
2019-11-02 12:09:42 +0900

    대시보드 페이지 꾸미기

--- client/src/pages/dashboard/dashboard.jsx
+++ client/src/pages/dashboard/dashboard.jsx
@@ -18,11 +18,24 @@ class DashBoard extends Component {
   state = {
     subscription: null,
     selectedValue: new Date(),
-    staticSubscribeArr: []
+    staticSubscribeArr: [],
+    isFullMode: true,
+  };
+
+  showFullMode = (e) =>{
+    e.preventDefault();
+
+    this.setState({
+      isFullMode: true
+    });
   };
 
   handleChange = (e) => {
-    this.setState({ selectedValue: e.format('YYYY-MM-DD') });
+    console.log('handleChange');
+    this.setState({
+      selectedValue: e.format('YYYY-MM-DD'),
+      isFullMode: false
+    });
   };
 
   convertDate = () => {
@@ -70,17 +83,18 @@ class DashBoard extends Component {
                 <Calendar date={selectedValue} handleChange={this.handleChange} data={subscription} />
               </div>
               {/* 달력 - 일 */}
-              {/*<div className="list">*/}
+              {/* <div className="list">*/}
               {/*  <span className="component-title"><u>일별 결제일 정보</u></span>*/}
               {/*  <List date={this.convertDate()} data={subscription} />*/}
-              {/*</div>*/}
+              {/* </div>*/}
             </div>
 
             {/* 구독중인 서비스 list */}
             <div className="col-md-6 dashboard-inner">
               <div className='categories'>
-                <div className="categories-component-title"> 10월 전체 구독 결제 정보 </div>
-                <Categories data={subscription} />
+                <span className="categories-component-title"> {new Date().getMonth() + 1}월 구독 결제 정보 </span>
+                <button onClick={this.showFullMode} type="button" className="btn btn-outline-info"> 전체 정보 보기 </button>
+                <Categories data={subscription} date={this.convertDate()} isFullMode={this.state.isFullMode}/>
               </div>
             </div>
 
