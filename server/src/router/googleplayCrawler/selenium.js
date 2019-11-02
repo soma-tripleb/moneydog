0737964f1c4ae6046e237fb468b38d549581bf21
JaeyeonKIM <foodsksms@gmail.com>
2019-11-03 01:29:40 +0900

    selenium으로 비밀번호 입력받기

--- server/src/router/googleplayCrawler/selenium.js
+++ server/src/router/googleplayCrawler/selenium.js
@@ -8,11 +8,12 @@ const getLogin = async () => {
   const driver = await new Builder().forBrowser('chrome').build();
   try {
     await driver.get('https://play.google.com/store/account/orderhistory');
-    await driver.findElement(By.css('#identifierId')).sendKeys('testid');
+    await driver.findElement(By.css('#identifierId')).sendKeys('test');
     await driver.findElement(By.css('#identifierNext')).click();
-    await driver.wait(until.elementLocated(By.css('#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input')));
-    await driver.findElement(By.css('#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input')).sendKeys('testpw');
-    await driver.findElement(By.id('#passwordNext')).click();
+    await driver.wait(until.elementLocated(By.css('#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input')), 1000).then((el) => el.sendKeys('testpwd'));
+    // await driver.findElement(By.css('#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input')).sendKeys('testpw');
+    // await driver.findElement(By.id('#passwordNext')).click();
+    await driver.wait(until.elementLocated(By.id('#passwordNext')), 1000).then((el) => el.click());
   } catch (e) {
     console.log(`error : ${e}`);
   }
