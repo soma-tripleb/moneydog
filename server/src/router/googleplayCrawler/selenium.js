424d1469a463a4e9d7fee344999c5a252f0226c8
JaeyeonKIM <foodsksms@gmail.com>
2019-11-05 23:51:10 +0900

    #107 list의 WebElement조회
    - Element가 정상적으로 들어갔다. 이제 1개의 Element에 대한 parsing을 해야한다.

--- server/src/router/googleplayCrawler/selenium.js
+++ server/src/router/googleplayCrawler/selenium.js
@@ -8,10 +8,10 @@ const getLogin = async () => {
   const driver = await new Builder().forBrowser('chrome').build();
   try {
     await driver.get('https://play.google.com/store/account/subscriptions');
-    await driver.findElement(By.css('#identifierId')).sendKeys('testpw');
+    await driver.findElement(By.css('#identifierId')).sendKeys('id');
     await driver.findElement(By.css('#identifierNext')).click();
     await driver.sleep(1000);
-    await driver.wait(until.elementLocated(By.css('#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input')), 10000).then((el) => el.sendKeys('test'));
+    await driver.wait(until.elementLocated(By.css('#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input')), 10000).then((el) => el.sendKeys('pw'));
     await driver.sleep(300);
     await driver.wait(until.elementLocated(By.css('#passwordNext')), 10000).click();
     return driver;
@@ -22,11 +22,12 @@ const getLogin = async () => {
 
 const getSubscriptionList = async () => {
   const driver = await getLogin();
-  await driver.wait(until.elementLocated(By.css('#fcxH9b > div.WpDbMd > c-wiz:nth-child(4) > div > c-wiz > table > tbody')), 10000).then(() => console.log('render'));
-  const list = await driver.findElement(By.css('#fcxH9b > div.WpDbMd > c-wiz:nth-child(4) > div > c-wiz > table > tbody')).then((el) => el.findElements(By.className('hBukP')));
+  await driver.wait(until.elementLocated(By.css('#fcxH9b > div.WpDbMd > c-wiz > div > c-wiz > table > tbody')), 10000).then(() => console.log('render'));
+  const list = await driver.findElement(By.css('#fcxH9b > div.WpDbMd > c-wiz > div > c-wiz > table > tbody')).then((el) => el.findElements(By.className('bzdI0b')));
   console.log(list.length);
-  console.log(list[0].getAttribute('innerHTML'));
-}
+  const test = list[0];
+  test.getAttribute('innerHTML').then((html) => console.log(html));
+};
 
 getSubscriptionList();
 
