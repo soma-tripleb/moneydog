8546c065774e0e8dbe1f088f0ad3f9a3f931de5a
JaeyeonKIM <foodsksms@gmail.com>
2019-11-05 23:31:43 +0900

    before pull jy-issue-107

--- server/src/router/googleplayCrawler/selenium.js
+++ server/src/router/googleplayCrawler/selenium.js
@@ -8,15 +8,22 @@ const getLogin = async () => {
   const driver = await new Builder().forBrowser('chrome').build();
   try {
     await driver.get('https://play.google.com/store/account/orderhistory');
-    await driver.findElement(By.css('#identifierId')).sendKeys('testid');
+    await driver.findElement(By.css('#identifierId')).sendKeys('moneydogtest1');
     await driver.findElement(By.css('#identifierNext')).click();
     await driver.sleep(1000);
-    await driver.wait(until.elementLocated(By.css('#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input')), 10000).then((el) => el.sendKeys('testpw'));
+    await driver.wait(until.elementLocated(By.css('#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input')), 10000).then((el) => el.sendKeys('moneydog1234'));
     await driver.sleep(300);
     await driver.wait(until.elementLocated(By.css('#passwordNext')), 10000).click();
+    return driver;
   } catch (e) {
     console.log(`error : ${e}`);
   }
 }
 
-getLogin();
+const getSubscriptionList = async () => {
+  const driver = await getLogin();
+  const list = await driver.findElements(By.css('#fcxH9b > div.WpDbMd > c-wiz:nth-child(4) > div > c-wiz > table > tbody'));
+  console.log(JSON.stringify(list));
+}
+
+getSubscriptionList();
