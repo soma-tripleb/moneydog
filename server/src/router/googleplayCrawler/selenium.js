74cf79322bbdea461898a2291f8d5cb177ab25e0
jaeyeon kim <foodsksms@gmail.com>
2019-11-05 13:27:39 +0900

    #107 selenium으로 구독리스트 조회하는거 만드는중
    - 구독리스트까지는 이동했지만, 리스트 테이블을 query로 가져오는 부분이 잘안된다.

--- server/src/router/googleplayCrawler/selenium.js
+++ server/src/router/googleplayCrawler/selenium.js
@@ -8,15 +8,32 @@ const getLogin = async () => {
   const driver = await new Builder().forBrowser('chrome').build();
   try {
     await driver.get('https://play.google.com/store/account/orderhistory');
-    await driver.findElement(By.css('#identifierId')).sendKeys('testid');
+    await driver.findElement(By.css('#identifierId')).sendKeys('test');
     await driver.findElement(By.css('#identifierNext')).click();
     await driver.sleep(1000);
-    await driver.wait(until.elementLocated(By.css('#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input')), 10000).then((el) => el.sendKeys('testpw'));
-    await driver.sleep(300);
+    await driver.wait(until.elementLocated(By.css('#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input')), 10000).then((el) => el.sendKeys('test'));
+    await driver.sleep(1000);
     await driver.wait(until.elementLocated(By.css('#passwordNext')), 10000).click();
+    return driver;
   } catch (e) {
     console.log(`error : ${e}`);
   }
 }
 
-getLogin();
+// nodelist => table[0].childNodes
+const getSubscriptionList = async () => {
+  try {
+    const driver = await getLogin();
+    await driver.sleep(1000).then(() => console.log(`1초동안 sleep`));
+    const list = await driver.wait(until.elementLocated(By.css('#fcxH9b > div.WpDbMd > c-wiz > div > div.oOodye > table > tbody')), 10000).then((el) => {
+      return el.findElements(By.className('bzdI0b'));});
+    console.log(list);
+    driver.close();
+  } catch (e) {
+    console.log(`err : ${e}`);
+    driver.close();
+  }
+};
+
+getSubscriptionList();
+
