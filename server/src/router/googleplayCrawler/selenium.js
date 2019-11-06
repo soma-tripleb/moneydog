ed3a1278d45788ef3f0832984b96971c50404019
jaeyeon kim <foodsksms@gmail.com>
2019-11-06 15:55:18 +0900

    #107 renewal date포맷에 맞추기
    - renewal date 정규표현식 포맷에 맞추기

--- server/src/router/googleplayCrawler/selenium.js
+++ server/src/router/googleplayCrawler/selenium.js
@@ -8,10 +8,10 @@ const getLogin = async () => {
   const driver = await new Builder().forBrowser('chrome').build();
   try {
     await driver.get('https://play.google.com/store/account/subscriptions');
-    await driver.findElement(By.css('#identifierId')).sendKeys('id');
+    await driver.findElement(By.css('#identifierId')).sendKeys('test');
     await driver.findElement(By.css('#identifierNext')).click();
     await driver.sleep(1000);
-    await driver.wait(until.elementLocated(By.css('#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input')), 10000).then((el) => el.sendKeys('pw'));
+    await driver.wait(until.elementLocated(By.css('#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input')), 10000).then((el) => el.sendKeys('test'));
     await driver.sleep(300);
     await driver.wait(until.elementLocated(By.css('#passwordNext')), 10000).click();
     return driver;
@@ -36,8 +36,20 @@ const serviceParsing = async (element) => {
   result.name = await list[0].getText();
   result.plan = await list[1].getText();
   result.price = await element.findElement(By.className('EQjZle kSHPSd')).getText();
-  result.renewal = await element.findElement(By.className('l7Glx M7PYhd')).getText();
+  result.renewal = convertRenewalReg(await element.findElement(By.className('l7Glx M7PYhd')).getText());
+  console.log(JSON.stringify(result));
   return result;
 };
 
-getSubscriptionList();
+const convertRenewalReg = (renewal) => {
+  const renewalReg = /\d{4}\.\s\d{1,2}\.\s\d{1,2}/g;
+  return renewalReg.exec(renewal)[0];
+};
+
+const result = convertRenewalReg('2019. 11. 30.에 갱신됨');
+console.log(result);
+// getSubscriptionList();
+
+// price ₩4,400/개월
+// 2019. 11. 6.까지 무료, 이후 ₩59,000/년
+// 2019. 11. 30.에 갱신됨
