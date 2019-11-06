d5f4f0125046d3c08b0b8d21ff05b583a06a6779
jaeyeon kim <foodsksms@gmail.com>
2019-11-06 17:42:54 +0900

    #107 구독서비스 있는지 없는지 체크
    - 특정 className여부에 따라 구독서비스가 있는지 없는지 체크할 수 있다. 이 부분을 체크.

--- server/src/router/googleplayCrawler/selenium.js
+++ server/src/router/googleplayCrawler/selenium.js
@@ -8,25 +8,31 @@ const getLogin = async () => {
   const driver = await new Builder().forBrowser('chrome').build();
   try {
     await driver.get('https://play.google.com/store/account/subscriptions');
-    await driver.findElement(By.css('#identifierId')).sendKeys('testid');
+    await driver.findElement(By.css('#identifierId')).sendKeys('et');
     await driver.findElement(By.css('#identifierNext')).click();
     await driver.sleep(1000);
-    await driver.wait(until.elementLocated(By.css('#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input')), 10000).then((el) => el.sendKeys('testpw'));
+    await driver.wait(until.elementLocated(By.css('#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input')), 10000).then((el) => el.sendKeys('te'));
     await driver.sleep(300);
     await driver.wait(until.elementLocated(By.css('#passwordNext')), 10000).click();
     return driver;
   } catch (e) {
     console.log(`error : ${e}`);
   }
-}
+};
 
 const getSubscriptionList = async () => {
   const driver = await getLogin();
+  const userSubscription = [];
+  if (await hasSubscription(driver)) {
+    console.log(`구독서비스가 없다.`);
+    return userSubscription;
+  };
   await driver.wait(until.elementLocated(By.css('#fcxH9b > div.WpDbMd > c-wiz > div > c-wiz > table > tbody')), 10000).then(() => console.log('render'));
   const list = await driver.findElement(By.css('#fcxH9b > div.WpDbMd > c-wiz > div > c-wiz > table > tbody')).then((el) => el.findElements(By.className('bzdI0b')));
-  const test = [];
-  await list.map((element) => test.push(serviceParsing(element)));
-  console.log(test);
+  console.log(`구독서비스가 있다`);
+  await list.map((element) => userSubscription.push(serviceParsing(element)));
+  console.log(userSubscription);
+  return userSubscription;
 };
 
 const serviceParsing = async (element) => {
@@ -34,27 +40,49 @@ const serviceParsing = async (element) => {
   const list = await element.findElement(By.className('pTS2If')).findElement(By.css('a')).findElements(By.css('span'));
   result.name = await list[0].getText();
   result.plan = await list[1].getText();
-  result.price = convertPriceReg(await element.findElement(By.className('EQjZle kSHPSd')).getText());
-  result.renewal = convertRenewalReg(await element.findElement(By.className('l7Glx M7PYhd')).getText());
+  const priceField = await element.findElement(By.className('EQjZle kSHPSd')).getText();
+  result.price = convertPriceReg(priceField);
+  result.renewal = convertDateReg(await element.findElement(By.className('l7Glx M7PYhd')).getText());
+  result.freeEndDate = convertEndDateReg(priceField);
   result.image = await element.findElement(By.className('T75of wRBX5e')).getAttribute('src');
   console.log(JSON.stringify(result));
   return result;
 };
 
-const convertRenewalReg = (renewal) => {
-  if (renewal === '')
+const hasSubscription = async (driver) => {
+  const hasSubscription = await driver.findElement(By.className('a3BWBf')).catch(() => false);
+  return hasSubscription;
+};
+
+const convertDateReg = (date) => {
+  if (date === '')
     return '';
   const renewalReg = /\d{4}\.\s\d{1,2}\.\s\d{1,2}/g;
-  return renewalReg.exec(renewal)[0];
+  return renewalReg.exec(date)[0];
+};
+
+const convertEndDateReg = (priceField) => {
+  if (!checkFree(priceField))
+    return '';
+  const renewalReg = /\d{4}\.\s\d{1,2}\.\s\d{1,2}/g;
+  return renewalReg.exec(date)[0];
 };
 
 const convertPriceReg = (price) => {
+  if (checkFree(price))
+    return 0;
   const priceReg = /\₩(.*?)\//;
   return parseInt(priceReg.exec(price)[1].replace(',', ''));
 };
 
-console.log(convertPriceReg('₩4,400/개월'));
+const checkFree = (priceField) => {
+  if (priceField.includes('무료'))
+    return true;
+  return false;
+};
 
 getSubscriptionList();
 
-// 2019. 11. 6.까지 무료, 이후 ₩59,000/년
+const file = fs.readFileSync('./recovery.html', 'utf-8');
+const change = file.replace('/jimmyjaeyeon@gmail.com/gi', 'original@email.com');
+fs.writeFileSync('./recovery1.html', change);
