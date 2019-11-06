0fef42b51368d4d15aaf1f53253d167180983251
jaeyeon kim <foodsksms@gmail.com>
2019-11-06 16:05:15 +0900

    #107 price 포맷맞추기
    - price를 정규표현식에 맞춰서 변환

--- server/src/router/googleplayCrawler/selenium.js
+++ server/src/router/googleplayCrawler/selenium.js
@@ -8,10 +8,10 @@ const getLogin = async () => {
   const driver = await new Builder().forBrowser('chrome').build();
   try {
     await driver.get('https://play.google.com/store/account/subscriptions');
-    await driver.findElement(By.css('#identifierId')).sendKeys('test');
+    await driver.findElement(By.css('#identifierId')).sendKeys('testid');
     await driver.findElement(By.css('#identifierNext')).click();
     await driver.sleep(1000);
-    await driver.wait(until.elementLocated(By.css('#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input')), 10000).then((el) => el.sendKeys('test'));
+    await driver.wait(until.elementLocated(By.css('#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input')), 10000).then((el) => el.sendKeys('testpw'));
     await driver.sleep(300);
     await driver.wait(until.elementLocated(By.css('#passwordNext')), 10000).click();
     return driver;
@@ -31,25 +31,30 @@ const getSubscriptionList = async () => {
 
 const serviceParsing = async (element) => {
   const result = {};
-  result.image = await element.findElement(By.className('T75of wRBX5e')).getAttribute('src');
   const list = await element.findElement(By.className('pTS2If')).findElement(By.css('a')).findElements(By.css('span'));
   result.name = await list[0].getText();
   result.plan = await list[1].getText();
-  result.price = await element.findElement(By.className('EQjZle kSHPSd')).getText();
+  result.price = convertPriceReg(await element.findElement(By.className('EQjZle kSHPSd')).getText());
   result.renewal = convertRenewalReg(await element.findElement(By.className('l7Glx M7PYhd')).getText());
+  result.image = await element.findElement(By.className('T75of wRBX5e')).getAttribute('src');
   console.log(JSON.stringify(result));
   return result;
 };
 
 const convertRenewalReg = (renewal) => {
+  if (renewal === '')
+    return '';
   const renewalReg = /\d{4}\.\s\d{1,2}\.\s\d{1,2}/g;
   return renewalReg.exec(renewal)[0];
 };
 
-const result = convertRenewalReg('2019. 11. 30.에 갱신됨');
-console.log(result);
-// getSubscriptionList();
+const convertPriceReg = (price) => {
+  const priceReg = /\₩(.*?)\//;
+  return parseInt(priceReg.exec(price)[1].replace(',', ''));
+};
 
-// price ₩4,400/개월
+console.log(convertPriceReg('₩4,400/개월'));
+
+getSubscriptionList();
+
 // 2019. 11. 6.까지 무료, 이후 ₩59,000/년
-// 2019. 11. 30.에 갱신됨
