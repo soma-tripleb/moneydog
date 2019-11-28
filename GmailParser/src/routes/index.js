122f51c24de3828738bd08b5479b3c2452ac5dc6
parkyounghwan <dudrnxps1@gmail.com>
2019-11-28 22:55:21 +0900

    #81, swagger install

--- GmailParser/src/routes/index.js
+++ GmailParser/src/routes/index.js
@@ -1,6 +1,39 @@
+/**
+ * @swagger
+ * tags:
+ *   name: User
+ *   description: 사용자 정보 가져오기
+ */
+
 import express from 'express';
 const router = express.Router();
 
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     description: Check API Server Start
+ *     produces:
+ *      - application/json
+ *     summary: index
+ *     tags: [Index]
+ *     parameters:
+ *       - in: query
+ *         name: id
+ *         type: string
+ *         enum: [cjung, gglee, etc..]
+ *         description: |
+ *          사용자 아이디 전달
+ *     responses:
+ *       200:
+ *         description: 성공
+ *       403:
+ *         $ref: '#/components/res/Forbidden'
+ *       404:
+ *         $ref: '#/components/res/NotFound'
+ *       500:
+ *         $ref: '#/components/res/BadRequest'
+ */
 router.get('/', (req, res, next) => {
   res.send('Hello Gmail Parser');
 });
